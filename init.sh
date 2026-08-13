2faf876001ce723bd5154d352397d24c8fcdcb2a
Phen Chua <89496400+phenchua29@users.noreply.github.com>
2026-08-13 12:44:01 +0700

    feat(init): install ansible-lint on arch

--- init.sh
+++ init.sh
@@ -3,4 +3,9 @@
 echo "Installing Ansible dependencies..."
 ansible-galaxy install -r ./requirements.yml --verbose
 
+if [ -f /etc/arch-release ]; then
+  echo "Installing ansible-lint..."
+  sudo pacman -Sy --noconfirm ansible-lint
+fi
+
 echo "Done"
